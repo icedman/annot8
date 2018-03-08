24b0c5c74db36f83e1bc65051517d53425934f4d
Marvin Sanchez <m4rvin2005@gmail.com>
2018-03-08 09:36:23 +0800

    add debug & toolbar

---
+++ src/Toolbar.vue
@@ -0,0 +1,16 @@
+<template>
+    
+<div v-if="a8.selection">
+  <a class="annot8-button" @click="a8.annotate()">Annotate</a>
+  <a class="annot8-button" @click="a8.annotate('green')">Green Tag</a>
+</div>
+
+</template>
+
+<script>
+export default {
+    props: {
+        a8: Object
+    }
+}
+</script>
